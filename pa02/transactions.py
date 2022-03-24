ef5e9f9655ab47378e84f546ea4901f41210e679
Yiwen789 <luoy2@Carleton.edu>
2022-03-24 14:17:45 -0400

    added contructor and select_one for transactions class

--- pa02/transactions.py
+++ pa02/transactions.py
@@ -11,16 +11,43 @@ This app will store the data in a SQLite database ~/transactions.db
 
 import sqlite3
 
-class Transactions ():
-    '''Transactions represens a table of transactions'''
+def to_trans_dict(trans_tuple):
+    trans = {'rowid': trans_tuple[0], 
+    'item_number': trans_tuple[1], 
+    'amount': trans_tuple[2], 
+    'category': trans_tuple[3], 
+    'date': trans_tuple[4], 
+    'description': trans_tuple[5]}
+    return trans
 
+class Transactions ():
+    '''Transactions represents a table of transactions'''
+    # author: Yiwen
+    def __init__(self, dbfile):
+      con = sqlite3.connect(self.dbfile)
+      cur = con.cursor()
+      cur.execute('''CREATE TABLE IF NOT EXISTS transactions 
+                (item_number numeric, amount numeric, category text, date date, description text)''')
+      con.commit()
+      con.close()
+      self.dbfile = dbfile
+
+    # author: Yiwen
+    def select_one(self, rowid):
+      con= sqlite3.connect(self.dbfile)
+      cur = con.cursor()
+      cur.execute("SELECT rowid,* from categories where rowid=(?)",(rowid,) )
+      tuples = cur.fetchall()
+      con.commit()
+      con.close()
+      return to_trans_dict(tuples[0])
 
     #author: Jiefang Li
     def Update(self, rowid, item) :
-        '''update the content of a certian transaction'''
+        '''update the content of a certian transactions'''
         con= sqlite3.connect(self.dbfile)
         cur = con.cursor()
-        cur.execute('''UPDATE transactions
+        cur.execute('''UPDATE transaction
                         SET item#=(?), amount=(?), category=(?), date=(?), description=(?)
                         WHERE rowid=(?);
         ''',(item['item#'],item['amount'],item['category'],item['date'],item['description'],rowid))
