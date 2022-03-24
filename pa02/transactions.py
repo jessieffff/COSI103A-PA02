c06dd6516a7940b02cb6833ab3f92326afcf32fa
jessiefff <jiefangli0605@gmail.com>
2022-03-24 10:18:32 -0400

    update tractor and transactions?

--- pa02/transactions.py
+++ pa02/transactions.py
@@ -11,6 +11,17 @@ This app will store the data in a SQLite database ~/transactions.db
 
 import sqlite3
 
+def to_trans_dict(trans_tuple):
+    ''' trans is a category tuple (rowid, name, desc)'''
+    trans = {'rowid':trans_tuple[0], 'item#':trans_tuple[1], 'amount':trans_tuple[2], 
+            'category':trans_tuple[3], 'data':trans_tuple[4], 'description':trans_tuple[5]}
+    return trans
+
+def to_trans_dict_list(trans_tuples):
+    ''' convert a list of category tuples into a list of dictionaries'''
+    return [to_trans_dict(trans) for trans in trans_tuples]
+
+
 class Transactions ():
     '''Transactions represens a table of transactions'''
 
@@ -26,3 +37,5 @@ class Transactions ():
         ''',(item['item#'],item['amount'],item['category'],item['date'],item['description'],rowid))
         con.commit()
         con.close()
+    
+    #Do we need to add a new function named grouped by? For example group by category, or month?
